95516f013bea7e01245a226a9bae03b1598bee1a
Greg Huels <greg@heapanalytics.com>
2020-06-20 09:08:37 -0500

    Remove dist from repo

--- dist/helpers/getVersion.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function getVersion() {
-    return require('../../package.json').version;
-}
-exports["default"] = getVersion;
-//# sourceMappingURL=getVersion.js.map
