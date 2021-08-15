8b216479ee526e7d1cd7e9524bca2956c64ef668
Greg Huels <greg.huels@gmail.com>
2021-08-15 16:44:33 -0500

    Fix spawn

--- src/helpers/subprocess.ts
+++ src/helpers/subprocess.ts
@@ -16,21 +16,10 @@ export const exec = async (...cmd: Array<string>) => {
 }
 
 export const spawn = async (...cmd: Array<string>) => {
-  const p = Deno.run({ cmd, stdout: 'piped', stdin: 'piped', stderr: 'piped' });
-  const [status, stdout, stderr] = await Promise.all([
-    p.status(),
-    p.output(),
-    p.stderrOutput()
-   ]);
-  p.close();
-  if (status.code === 0) {
-    await Deno.stdout.write(stdout);
-  } else {
-    if (stderr) {
-      const errorString = decode(stderr);
-      console.error(errorString);
-      Deno.exit(status.code)
-    }
+  const p = Deno.run({ cmd });
+  const status = await p.status();
+  if (status.code !== 0) {
+    Deno.exit(status.code)
   }
   return status.code;
 }
