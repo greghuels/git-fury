1a87fb4f27cb8a9e1101984286e62227bb77e6eb
Greg Huels <greg@heapanalytics.com>
2022-12-18 06:40:46 -0600

    refactor to simplify code

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-pushd $(git rev-parse --show-toplevel)
+pushd "$(git rev-parse --show-toplevel)"
 if [ $(basename $(pwd)) != "git-fury" ]; then
   echo "must be run from git-fury project"
   exit 1
 fi
 
-export version=$(cat ./version.ts | sed -e 's/export default "//' | sed -e 's/";//')
+export version="$(cat ./version.ts | sed -e 's/export default "//' | sed -e 's/";//')"
 if [ ! -d "./target" ]; then
   mkdir target
 fi
-rm -rf target/$version
-mkdir target/$version
+rm -rf "target/$version"
+mkdir "target/$version"
 
 export targets=( "aarch64-apple-darwin" "x86_64-apple-darwin" "x86_64-unknown-linux-gnu" )
 
